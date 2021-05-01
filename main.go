93bee6cee1619377f45036d6992363555e10b8a1
Puneet Garg <garpunee@amazon.com>
2021-05-01 10:46:52 +1000

    adding ec2metadata

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func handler(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, ec2metadata.InstanceID() , r.URL.Path[1:])
 }
 
 func main() {
